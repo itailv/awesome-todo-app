a74d5e1ee0ade31631842fc7df2a136c173d77b4
Itai <itailavie.main@gmail.com>
2020-11-01 11:58:51 +0200

    ⚡️ Remove todo confirmation dialog

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Checkbox, Tag, Typography } from "antd";
+import { Checkbox, Tag, Typography, Modal } from "antd";
 
 import { Category, TodoCompleted, TodoContent, TodoId, TodoPriorityEnum } from "../../api";
 import CloseIcon from "../../icons/CloseIcon";
 import style from './Todo.module.css';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
 
 
 const TodoPriorityColors = new Map<TodoPriorityEnum, string>([
@@ -29,10 +30,24 @@ const TodoItem: React.FC<TodoProps> = ({
     onToggleTodo, isToggleTodoLoading,
     onRemoveTodo, isRemoveTodoLoading,
 }) => {
+
+    const onRemoveClick = (): void => {
+        Modal.confirm({
+            title: 'Delete this todo?',
+            okText: 'Delete',
+            okType: 'danger',
+            cancelText: 'Cancel',
+            style: { marginTop: 100 },
+            onOk() {
+                onRemoveTodo(id);
+            },
+        });
+    };
+
     return (
         <div className={style.todoContainer} style={completed ? { opacity: "50%" } : {}}>
             <div className={style.titleRow}>
-                <CloseIcon className={style.removeBtn} onClick={() => onRemoveTodo(id)} />
+                <CloseIcon className={style.removeBtn} onClick={onRemoveClick} />
                 <Typography.Text delete={completed} className={style.titleText}>{content}</Typography.Text>
                 <div className={style.checkbox}>
                     <Checkbox disabled={isToggleTodoLoading || isRemoveTodoLoading} checked={completed} onClick={() => onToggleTodo(id)} />
